464309190df5ae6f860798b02b30af4ce4ae2d45
anshika <guptaap_3@rknec.edu>
2023-11-28 10:40:00 +0530

    Create Song database model

---
+++ database/seeders/SongSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\Song;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SongSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+	    //
+	    // Seed a few songs
+        Song::create([
+            'artist' => 'CHANYEOL',
+            'title' => 'Stay with Me',
+            // 'time' => '3:12',
+            // 'tempo' => 125,
+        ]);
+
+        Song::create([
+            'artist' => 'Taylor Swift',
+            'title' => 'Lover',
+            // 'time' => '3:41',
+            // 'tempo' => 69,
+        ]);
+    }
+}
